0e7b7caddd655d16bf965a7c5ac4137a2641145d
Ömer Faruk Altun <altunomerfarukk@gmail.com>
2026-03-14 22:34:24 +0300

    1.0

--- main.py
+++ main.py
@@ -2,3 +2,22 @@ import turtle
 
 GameScreen = turtle.Screen()
 GameScreen.bgcolor("light blue")
+turtle.write("Skor")
+tospik = turtle.Turtle()
+tospik.color("gray")
+tospik.forward(100)
+tospik.left(90)
+tospik.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.done()
