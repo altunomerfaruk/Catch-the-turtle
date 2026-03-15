ca9c5626ba5ed205ae673c1267f311b464b61029
Ömer Faruk Altun <altunomerfarukk@gmail.com>
2026-03-15 15:57:57 +0300

    OnTimer

--- main.py
+++ main.py
@@ -2,22 +2,27 @@ import turtle
 
 GameScreen = turtle.Screen()
 GameScreen.bgcolor("light blue")
-turtle.write("Skor")
+style = ('Courier', 30, 'italic')
+
+def countdown(time):
+    GameScreen.onclick(None)  # disable click until countdown completes
+    tospik.clear()
+
+    if time > 0:
+        tospik.write(time, align='center', font=style)
+        GameScreen.ontimer(lambda: countdown(time - 1), 1000)
+    else:
+        tospik.write("Click Screen", align='center', font=style)
+        GameScreen.onclick(lambda x, y: countdown(30))
+
+
+
 tospik = turtle.Turtle()
-tospik.color("gray")
-tospik.forward(100)
-tospik.left(90)
-tospik.forward(100)
+tospik.hideturtle()
+tospik.write("Click Screen", align='center', font=style)
+GameScreen.onclick(lambda x, y: countdown(30))
 
 
 
+GameScreen.mainloop()
 
-
-
-
-
-
-
-
-
-turtle.done()
